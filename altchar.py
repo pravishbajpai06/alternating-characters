b4a168d774b349797e0a417b4c54fa7c0aacb8f1
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-08-29 00:25:37 +0530

    Update altchar.py

--- altchar.py
+++ altchar.py
@@ -1,1 +1,13 @@
+'''You are given a string containing characters A  and B only. Your task is to change it into a string such that there are no matching adjacent characters.
+To do this, you are allowed to delete zero or more characters in the string.
+Your task is to find the minimum number of required deletions.
+For example, given the string s=AABAAB, remove an A  at positions 0 and 3 to make  in  deletions.'''
+
+def altchar():
+  c=0
+  for  in range(1,len(s)):
+    if s[i]==s[i-1]:
+      c=c+1
+  return(c)    
+    
 
